f427618baf0969d0d80388a02b464708949abed6
Kauan de Oliveira Carvalho <kauan.carvalho@corregedoria.df>
2025-09-09 13:11:13 -0300

    implementação do swagger

--- api-usuarios/src/app.js
+++ api-usuarios/src/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const User = require('./models/user.models');
-const userRoutes = require('./routes/userRouter')
+const userRoutes = require('./routes/userRouter');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
 
 const app = express();
 app.use(express.json());
 
-app.use('/users', userRoutes)
+app.use('/users', userRoutes);
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 module.exports = app;
