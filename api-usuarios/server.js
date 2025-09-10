f202935e8a46f7bfd5c3f6c675d1342a56c23852
Kauan de Oliveira Carvalho <kauan.carvalho@corregedoria.df>
2025-09-10 14:17:25 -0300

    retirada dos consoles log

--- api-usuarios/server.js
+++ api-usuarios/server.js
@@ -5,11 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 sequelize.sync()
   .then(() => {
-    console.log('Banco sincronizado com sucesso!');
     app.listen(PORT, () => {
-      console.log(` Servidor rodando na porta ${PORT}`);
     });
   })
   .catch(err => {
-    console.error(' Erro ao sincronizar banco:', err);
   });
